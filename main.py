50dbef24a608c666f3f35c2a912f61f1614f82ba
Carlos Jusino <carlosdjusino@gmail.com>
2026-06-24 20:38:57 +0000

    nonprofits database schema set up to inclue name, urls, tags

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ import json
 myPublicApiKey = 'pk_live_c5228e363c68a2c177763aea16b20d47'
 cause = "animals"
 
+my_params = {
+  'apiKey': myPublicApiKey,
+  'take': 30, # number of results
+  'page': 1
+}
+
 # GET request
-response = requests.get("https://partners.every.org/v0.2/search/" + cause + "?apiKey=" + myPublicApiKey)
-animalNonProfits = response.json()
+response = requests.get("https://partners.every.org/v0.2/browse/" + cause, params = my_params)
+result = response.json()
 
 # create a dataframe
-df = pd.DataFrame.from_dict(animalNonProfits)
-print(df)
+df = pd.DataFrame(result['nonprofits'], columns = ['name', 'description', 'profileUrl', 'websiteUrl', 'location', 'tags'])
+
+# turn tags into a string
+df['tags'] = df['tags'].apply(json.dumps)
+# print(df)
 
 engine = db.create_engine('sqlite:///MatchMission.db')
 
-# df.to_sql('NonProfits', con=engine, if_exists='replace', index=False)
+df.to_sql('NonProfits', con=engine, if_exists='replace', index=False)
 
-# with engine.connect() as connection:
-#    query_result = connection.execute(db.text("SELECT * FROM NonProfits;")).fetchall()
-#    print(pd.DataFrame(query_result))
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM NonProfits;")).fetchall()
+   print(pd.DataFrame(query_result))
