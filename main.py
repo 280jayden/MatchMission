75842bfa18e4499f2ef476de25054b37a12f3f67
Carlos Jusino <carlosdjusino@gmail.com>
2026-06-24 22:36:12 +0000

    planned out the order of function calls in main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import sqlalchemy as db
 from dotenv import load_dotenv
+from questions import run_quiz
 from fetch_orgs import fetch_orgs
+
 
 if __name__ == '__main__':
   load_dotenv()
@@ -9,5 +11,31 @@ if __name__ == '__main__':
 
   engine = db.create_engine('sqlite:///MatchMission.db')
 
-  fetch_orgs("animals", 25, engine)
-  fetch_orgs("cancer", 10, engine)
+  user_profile = {'name': 'Carlos Jusino',
+    # in descending order
+    'top_5_tags_to_search': ['animals', 'culture', 'immigrants', 'housing', 'youth'],
+    'animals' : 0.98,
+    'culture' : 0.67,
+    'immigrants' : 0.65,
+    'housing' : 0.58,
+    'youth' : 0.55,
+    'music' : 0.44,
+    'health' : 0.32,
+    'freepress' : 0.21
+    # omit categories that are < 0.15 match
+  }
+  
+  # Welcome & 10 Question Quiz
+  name, responses = run_quiz()
+
+  # user_profile = llm weight ranking(responses)
+    # tell llm to take the responses, give weights, output a 
+    # json in the format of user_profile above
+
+  # fetch_orgs(tag) for t in tags_to_search
+    # get in batches of 30, 25, 20, 15, 10 (5 total fetches)
+    # will be sorted in db by a 'match score'
+    # fetch_orgs("animals", 25, engine)
+    # fetch_orgs("cancer", 10, engine)
+  
+  # display_top_5_matches()
