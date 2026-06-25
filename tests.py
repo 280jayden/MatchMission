1691b687b522d785994512e08ba25e185726349a
Carlos Jusino <carlosdjusino@gmail.com>
2026-06-25 20:17:53 +0000

    added algorithm and invalid fetch tests in test.py

---
+++ tests.py
@@ -0,0 +1,18 @@
+import pytest
+import sqlalchemy as db
+from fetch_orgs import *
+
+def test_algorithm():
+    user_causes = {'animals': 0.99, 'research': 0.85, 'education': 0.65, 'environment': 0.56}
+    nonprofit_tags = ['animals', 'environment', 'dance']
+    result = 10 * 0.99 + 10 * 0.56 + 10 * 0
+    assert algorithm(user_causes, nonprofit_tags) == result
+
+
+def test_invalid_fetch():
+    user_causes = {'animals': 0.99, 'research': 0.85, 'education': 0.65, 'environment': 0.56}
+    engine = db.create_engine('sqlite:///MatchMission.db')
+
+    with pytest.raises(Exception): # if code returns successfully, test fails
+        fetch_orgs(user_causes, "invalid_cause", 15, engine)
+
