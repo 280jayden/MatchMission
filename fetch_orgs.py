7d19831e302c30f3dcd69482e5e4288f673763ab
Carlos Jusino <carlosdjusino@gmail.com>
2026-06-25 19:51:02 +0000

    error handling for fetch_orgs function

--- fetch_orgs.py
+++ fetch_orgs.py
@@ -4,9 +4,83 @@ import pandas as pd
 import sqlalchemy as db
 import json
 
+
+cause_list = {
+    "aapi-led",
+    "adoption",
+    "afghanistan",
+    "animals",
+    "art",
+    "athletics",
+    "autism",
+    "black-led",
+    "buddhism",
+    "cancer",
+    "cats",
+    "christianity",
+    "climate",
+    "conservation",
+    "coronavirus",
+    "culture",
+    "dance",
+    "disabilities",
+    "disease",
+    "dogs",
+    "education",
+    "environment",
+    "filmandtv",
+    "food-security",
+    "freepress",
+    "gender-equality",
+    "health",
+    "hinduism",
+    "housing",
+    "humans",
+    "hurricane-ian",
+    "immigrants",
+    "indigenous-led",
+    "indigenous-peoples",
+    "islam",
+    "judaism",
+    "justice",
+    "latine-led",
+    "legal",
+    "lgbt",
+    "libraries",
+    "mental-health",
+    "museums",
+    "music",
+    "oceans",
+    "parks",
+    "poverty",
+    "racial-justice",
+    "radio",
+    "refugees",
+    "religion",
+    "research",
+    "science",
+    "seniors",
+    "space",
+    "theater",
+    "transgender",
+    "ukraine",
+    "veterans",
+    "votingrights",
+    "water",
+    "wildfires",
+    "wildlife",
+    "women-led",
+    "womens-health",
+    "youth",
+}
+
+
 def fetch_orgs(user_causes, cause, num_of_results, engine):
     myEveryorgApiKey = os.getenv('EVERYORG_KEY')
-
+    
+    if cause not in cause_list:
+        raise Exception("Invalid cause for every.org api search.")
+    
     my_params = {
     'apiKey': myEveryorgApiKey,
     'take': num_of_results,
