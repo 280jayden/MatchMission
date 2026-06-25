5cd3dcce609b2b66302fce66a6216b14082bc973
Carlos Jusino <carlosdjusino@gmail.com>
2026-06-25 19:03:43 +0000

    added algorithm, score column in db, select/display top 3 orgs functions

--- fetch_orgs.py
+++ fetch_orgs.py
@@ -22,12 +22,39 @@ def fetch_orgs(cause, num_of_results, engine):
 
     # turn tags into a string
     df['tags'] = df['tags'].apply(json.dumps)
-    # print(df)
+    df['score'] = 0
 
-
+    # specify the dataframe, selecting primary key in dataframe and in sqlite so that dupes don't get added
     df.to_sql('NonProfits', con=engine, if_exists='append', index=False)
 
     with engine.connect() as connection:
     # TODO: how to prevent duplicate entries
+        # add a GROUP BY
         query_result = connection.execute(db.text("SELECT * FROM NonProfits;")).fetchall()
         print(pd.DataFrame(query_result))
+# check if duplicates
+
+def algorithm(user_causes, nonprofit_tags):
+    # input: dict, list
+    score = 0
+    for tag in nonprofit_tags:
+        score += 10 * user_causes.get(tag, 0)
+    return score
+
+
+def select_orgs(engine):
+    with engine.connect() as connection:
+        result = connection.execute(db.text("SELECT * FROM NonProfits ORDER BY score DESC LIMIT 3"))
+        nonprofits = result
+    return result
+
+def display_orgs(nonprofits):
+    for nonprofit in nonprofits:
+        print(nonprofit[0], '--', nonprofit[4]) # Name
+        print('-' * 50)
+        print("Description: ", nonprofit[1])
+        print("Profile URL: ", nonprofit[2])
+        print("Website URL: ", nonprofit[3])
+        # print("Why this fits: ") # call Gemini API for summary
+        print()
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import sqlalchemy as db
 from dotenv import load_dotenv
 from questions import run_quiz
-from fetch_orgs import fetch_orgs
+from fetch_orgs import *
 
 
 if __name__ == '__main__':
@@ -28,13 +28,16 @@ if __name__ == '__main__':
   }
   
   # Welcome & 10 Question Quiz
-  name, responses = run_quiz()
+  fetch_orgs("animals", 10, engine)
+  nonprofits = select_orgs(engine)
+  display_orgs(nonprofits)
+  # name, responses = run_quiz()
 
   # user_profile = llm weight ranking(responses)
     # tell llm to take the responses, give weights, output a 
     # json in the format of user_profile above
 
-  fetch_orgs("animals", 25, engine)
+  # fetch_orgs("animals", 25, engine)
   # fetch_orgs(tag) for t in tags_to_search
     # get in batches of 30, 25, 20, 15, 10 (5 total fetches)
     # will be sorted in db by a 'match score'
