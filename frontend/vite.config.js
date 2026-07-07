6024ca429027e3f8f67d3283d96ece3d50efcee1
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-07 18:24:35 +0000

    (chore): set up flask & connected with react (currently holds placeholder code to check if it works)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: true, // this would allow all hosts but you can also put your specific codio url here if u want
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  }
 })
