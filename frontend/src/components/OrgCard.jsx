cc118826292b9071b343c8af650ceb15fc2c0486
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-08 22:58:29 +0000

    (feat): adapted orgcard to display passed in data

--- frontend/src/components/OrgCard.jsx
+++ frontend/src/components/OrgCard.jsx
@@ -1,23 +1,33 @@
 import "./OrgCard.css"
 import StarButton from "./StarButton";
 
-function OrgCard() { // would pass in a json later but for now....
+function OrgCard({ org }) { // would pass in a json later but for now....
   return (
     <div className="item-card org-card">
       <div className="org-top">
-        <img src="https://placehold.net/600x600.png" alt="placeholder image" className="org-img"/>
+        <img src={org.logoUrl} alt="organization logo" className="org-img"/>
 
         <div>
-          <h2>Org Name</h2>
-          <p style={{marginBottom: "50px"}}>description</p>
+          <h2>{org.name}</h2>
+          <h3>Based in {org.location}</h3>
+          <p style={{marginBottom: "50px"}}>{org.description}</p>
 
-          <h3>Why We Matched You</h3>
-          <p>description</p>
+          <h3>Tags:</h3>
+          <div>
+            {org.tags.map((tag, index) => (
+              <span key={index} className="tag">
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
 
       <div className="org-bot">
-        <button onClick={() => console.log("button pressed")} className="norm-button">
+        <button
+          onClick={() => window.open(org.websiteUrl, "_blank")}
+          className="norm-button"
+        >
           PROFILE
         </button>
 
@@ -32,3 +42,18 @@ function OrgCard() { // would pass in a json later but for now....
 }
 
 export default OrgCard
+
+
+/*
+nonprofit_card_data = {
+    "ein": "844229672",
+    "name": "Lil BUB's Big Fund",
+    "description": "Lil BUB is a one of a kind space cat. Since landing on Earth, she's raised over $1,000,000 for homeless pets nationwide...",
+    "logoUrl": "https://res.cloudinary.com/.../dsor2nxk97p87umlwhvt",
+    "coverImageUrl": "https://res.cloudinary.com/.../ipxxsfqxtt6skku7vh1z",
+    "location": "BLOOMINGTON, IN",
+    "websiteUrl": "https://www.goodjobbub.org",
+    "tags": ["dogs", "cats", "animals"]
+}
+
+ */
--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,6 +1,28 @@
 import OrgCard from "../components/OrgCard"
 
 function Result() {
+
+  const dummy = {"nonprofits": [
+    {
+      "description": "Lil BUB is a one of a kind space cat. Since landing on Earth, she's raised over $1,000,000 for homeless pets nationwide.. Lil BUB’s Big Fund exists to advocate for special needs companion animals and build a community that celebrates and fosters the",
+      "ein": "844229672",
+      "name": "Lil BUB's Big Fund",
+      "profileUrl": "https://www.every.org/lilbubsbigfund",
+      "logoUrl": "https://res.cloudinary.com/everydotorg/image/upload/c_lfill,w_24,h_24,dpr_2/c_crop,ar_24:24/q_auto,f_auto,fl_progressive/profile_pics/dsor2nxk97p87umlwhvt",
+      "coverImageUrl": "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_3840,q_80/profile_pics/ipxxsfqxtt6skku7vh1z",
+      "logoCloudinaryId": "profile_pics/dsor2nxk97p87umlwhvt",
+      "slug": "lilbubsbigfund",
+      "location": "BLOOMINGTON, IN",
+      "websiteUrl": "https://www.goodjobbub.org",
+      "tags": [
+        "dogs",
+        "cats",
+        "animals"
+      ]
+    }]
+  }
+
+
   return (
     <div>
       <h1 style={{textAlign:"center"}}>Results</h1>
@@ -8,8 +30,7 @@ function Result() {
       
       {/* later, will make it a map based on a dict or array that gets passed in */}
       <div className="card-container">
-        <OrgCard />
-        <OrgCard />
+        <OrgCard org = {dummy["nonprofits"][0]} />
       </div>
     </div>
   )
