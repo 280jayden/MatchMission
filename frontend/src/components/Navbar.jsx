0f8f96ffd2688e3aa376ecc33e313683ff529afe
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-08 01:58:17 +0000

    (feat): styled navbar and some layout/page background styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/mm_logo.png";
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <nav className="navbar"> 
-      <Link to="/">Home</Link>
-      <Link to="/quiz">Matcher</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/register">Register</Link>
-    </nav>
+    <div className="navbar">
+      <nav> 
+        <img src={logo} alt="MatchMission logo"/>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/quiz">Matcher</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </nav>
+    </div>
   )
 }
 
