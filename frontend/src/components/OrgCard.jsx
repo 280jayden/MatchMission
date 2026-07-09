3f51c7480760c09c44515ce3fe7b2abe4f6cb574
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-09 03:44:26 +0000

    (fix): fixing image resolution, fallback logo, styling

--- frontend/src/components/OrgCard.jsx
+++ frontend/src/components/OrgCard.jsx
@@ -2,11 +2,27 @@ import "./OrgCard.css"
 import StarButton from "./StarButton";
 import logo from "../assets/mm_logo.png";
 
-function OrgCard({ org }) { // would pass in a json later but for now....
+function OrgCard({ org }) { 
+
+  function resizeImg(url) {
+    if (!url) {
+      return ""
+    }
+
+    const parts = url.split("/");
+
+    parts[6] = 'c_lfill,w_600,h_600,dpr_2';
+    parts[7] = 'c_crop,ar_600:600';
+
+    // original ex https://res.cloudinary.com/everydotorg/image/upload/c_lfill,w_24,h_24,dpr_2/c_crop,ar_24:24/q_auto,f_auto,fl_progressive/profile_pics/xumk7i0itod4uilqg9vt
+
+    return parts.join("/");
+  }
+
   return (
     <div className="item-card org-card">
       <div className="org-top">
-        <img src={org.logoUrl || logo} alt="organization logo" className="org-img"/>
+        <img src={resizeImg(org.logoUrl) || logo} alt="organization logo" className="org-img"/>
 
         <div>
           <h2>{org.name}</h2>
@@ -14,7 +30,7 @@ function OrgCard({ org }) { // would pass in a json later but for now....
           <p style={{marginBottom: "50px"}}>{org.description}</p>
 
           <h3>Tags:</h3>
-          <div>
+          <div className="tag-container">
             {org.tags.map((tag, index) => (
               <span key={index} className="tag">
                 {tag}
