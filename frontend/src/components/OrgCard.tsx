4793fcd69b8e8cd112a8f1b8c361215574faae17
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-13 19:11:56 +0000

    chore: beginning to convert to ts. created organization type

--- frontend/src/components/OrgCard.tsx
+++ frontend/src/components/OrgCard.tsx
@@ -2,24 +2,15 @@ import "./OrgCard.css"
 import StarButton from "./StarButton";
 import logo from "../assets/mm_logo.png";
 import { useNavigate } from "react-router-dom";
+import { Organization } from "../types/organization";
 
-function OrgCard({ org, forceStarred = false }) { 
+type OrgCardProps = {
+  org: Organization;
+  forceStarred?: boolean;
+};
+
+function OrgCard({ org, forceStarred = false }: OrgCardProps) {
   const navigate = useNavigate();
-
-  function resizeImg(url) {
-    if (!url) {
-      return ""
-    }
-
-    const parts = url.split("/");
-
-    parts[6] = 'c_lfill,w_600,h_600,dpr_2';
-    parts[7] = 'c_crop,ar_600:600';
-
-    // original ex https://res.cloudinary.com/everydotorg/image/upload/c_lfill,w_24,h_24,dpr_2/c_crop,ar_24:24/q_auto,f_auto,fl_progressive/profile_pics/xumk7i0itod4uilqg9vt
-
-    return parts.join("/");
-  }
 
   return (
     <div className="item-card org-card">
