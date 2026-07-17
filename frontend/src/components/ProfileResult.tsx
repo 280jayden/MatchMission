2e5a935af47d663621b2f0a4829d41189459a8ab
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-17 23:11:30 +0000

    feat: added cute visual tags for main categories in result

--- frontend/src/components/ProfileResult.tsx
+++ frontend/src/components/ProfileResult.tsx
@@ -26,18 +26,24 @@ function ProfileResult() {
                 </h3>
         <div className="profile-result-container">
             <div className="profile-result-left">
-                <p>AI generated paragraph here</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque, lectus pellentesque sagittis blandit, ipsum sem condimentum quam, eu pellentesque diam dolor id magna. Cras tellus tortor, euismod eget imperdiet vitae, sollicitudin nec elit. Praesent sit amet sagittis dolor. In eu vehicula urna. Maecenas eget venenatis ex. Pellentesque nec hendrerit purus, sit amet volutpat justo. Praesent in hendrerit enim, at aliquet risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas sed velit a est rutrum malesuada. Etiam id erat pretium, mollis purus eu, mollis lectus. Aliquam eleifend nisi nec justo convallis, vel consequat dolor dictum. Fusce risus odio, dignissim id euismod sed, tristique at nunc. Sed suscipit magna at justo mattis, semper maximus purus malesuada. In vel sem ac lectus vehicula tincidunt sit amet vel lacus.</p>
+
                 <button onClick={scrollToResults}
                     className="result-button"
                 >SHOW ME<br/>MY MATCHES</button>
-
-                {userCategories.map((category) => (
-                      <AttributeTag
-                      key={category.tag}
-                      title={category.name}
-                      tagImageUrl={category.tagImageUrl}
-                      />
-                    ))}
+                
+                <div>
+                    <h3 style={{ textAlign: 'center' }}>Your Main Categories</h3>
+                    <div className="result-tag-container">
+                        {userCategories.map((category) => (
+                            <AttributeTag
+                            key={category.tag}
+                            title={category.name}
+                            tagImageUrl={category.tagImageUrl}
+                            />
+                            ))}
+                    </div>
+                </div>
             </div>
 
             <div className="result-weight-chart">
