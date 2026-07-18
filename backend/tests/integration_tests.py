452d864b92a047ba407126ca0aa8a5a624c2e953
Carlos Jusino <carlosdjusino@gmail.com>
2026-07-18 04:53:25 +0000

    test: added test for get directory orgs, fixed workflow

--- backend/tests/integration_tests.py
+++ backend/tests/integration_tests.py
@@ -5,16 +5,15 @@ from app import app
 
 client = app.test_client()
 
-def test_refresh_orgs_api():
-    print("Testing the API internally via Flask Test Client...\n")
+def test_orgs_directory_api():
+    # Tests /api/orgs/directory
 
     # GET request
-    response = client.get('/api/refresh_orgs')
-
+    response = client.get('/api/orgs/directory')
         
     # extract the json data from the response object
     data = response.get_json() 
-    # print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4))
 
     # checks if response is successful
     assert response.status_code == 200
