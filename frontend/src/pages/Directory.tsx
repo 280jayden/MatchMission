ab91f3a650b063d55677bc81d6f356ee35d474cc
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-20 16:34:14 +0000

    feat: made clickable filters

--- frontend/src/pages/Directory.tsx
+++ frontend/src/pages/Directory.tsx
@@ -1,25 +1,99 @@
-import AttributeTag from '../components/AttributeTag';
+import FilterChip from '../components/FilterChip';
 import categories from "../data/categories.json"
 import "../styles/Directory.css"
+import { useState, useEffect } from 'react';
+import OrgCard from '../components/OrgCard';
+import { API_URL } from '../config';
+import { GetOrgsResponse } from '../types/api';
+import { Organization } from '../types/organization';
 
 
 function Directory() {
+  const [orgs, setOrgs] = useState<Organization[]>([]);
+  const [selectedFilters, setSelectedFilters] = useState<Set<string>>(new Set());
+  // const [loading, setLoading] = useState(true);
+
+  // Fetch random orgs
+    const getAnyOrg = async () => { //This is a debug for now while i wait for other endpoint to be created
+        const response = await fetch(`${API_URL}/api/orgs/directory`, {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        const data: GetOrgsResponse = await response.json();
+
+        if (response.ok && 'directory' in data) {
+            setOrgs(data.directory);
+        } else if ('error' in data) {
+            console.log(data.error);
+        }
+
+        // setLoading(false);
+    };
+    
+    const toggleFilter = (filter: string) => {
+      setSelectedFilters(prev => {
+        const next = new Set(prev);
+        next.has(filter) ? next.delete(filter) : next.add(filter);
+        console.log("selected a new filter so we have " + Array.from(next));
+        return next;
+      })
+    }
+
+    const applyFilters = async () => { //Later this will work for an empty array to just get random ones again
+      // any of the selected buttons, add to filters array
+      // run the backend call to update the orgs
+
+      const response = await fetch(`${API_URL}/api/orgs/directory`, { //CHANGETHISCALLLATER
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ filters: Array.from(selectedFilters)}),
+        });
+
+      const data: GetOrgsResponse = await response.json();
+
+      if (response.ok && 'directory' in data) {
+            setOrgs(data.directory);
+        } else if ('error' in data) {
+            console.log(data.error);
+        }
+
+        // setLoading(false);
+    }
+
+
+
+    useEffect(() => {
+        getAnyOrg();
+    }, []);
+
     return (
         <div className="directory-container">
+          {/* FILTERS */}
             <div className="filter-bar">
-                <h3>FILTERS</h3>
+                <h2 style={{marginBottom: '0', marginTop:'30px'}}>FILTERS</h2>
+                <button className="apply-button" onClick={applyFilters}>Apply Filters</button>
                 {categories.map((category) => (
-                        <AttributeTag
+                        <FilterChip
                         key={category.tag}
                         title={category.name}
                         tagImageUrl={category.tagImageUrl}
+                        isSelected={selectedFilters.has(category.tag)}
+                        onSelect={()=> toggleFilter(category.tag)}
                         />
                         ))}
+                <button className="apply-button" onClick={applyFilters}>Apply Filters</button>
             </div>
-
+            
+            {/* ORGANIZATION CONTAINER */}
             <div className="directory-orgs page-background">
-              <h1>Directory</h1>
-              <p>later........</p>
+              <h1 style={{ textAlign: 'center'}}>Directory</h1>
+                <div className="card-container">
+                  {orgs.map((org, index) => (
+                    <OrgCard key={org.ein} org={org} isBestMatch={false} />
+                  ))}
+              </div>
             </div>
         </div>
     );
